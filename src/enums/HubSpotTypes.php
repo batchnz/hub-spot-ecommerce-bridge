45670c9e0e35bfe0de726116ecb6c8502bfbbfcd
danielsiemers <daniel@batch.nz>
2021-10-21 10:18:57 +1300

    feat(enums): added enum for hubspot types

---
+++ src/enums/HubSpotTypes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace batchnz\hubspotecommercebridge\enums;
+
+
+/**
+ * Class HubSpotTypes
+ * @package batchnz\hubspotecommercebridge\enums
+ *
+ * Defines the allowed HubSpot data types
+ */
+abstract class HubSpotTypes
+{
+    public const STRING = "STRING";
+    public const NUMBER = "NUMBER";
+    public const DATETIME = "DATETIME";
+    public const AVATAR_IMAGE = "AVATAR_IMAGE";
+}
