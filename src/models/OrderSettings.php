bffdba014ec463d7901320af48ea32e73aade52a
danielsiemers <daniel.siemers@gmail.com>
2022-11-29 17:09:22 +1300

    feat(settings): add settings to and remove ecommerce bridge settings
    Signed-off-by: danielsiemers <daniel.siemers@gmail.com>

--- src/models/OrderSettings.php
+++ src/models/OrderSettings.php
@@ -8,29 +8,30 @@ use yii\base\Exception;
 
 class OrderSettings extends Model
 {
+    private const ORDER_NUMBER = "craft_order_number";
+    private const ORDER_NAME = "dealname";
     private const ORDER_STAGE = "dealstage";
     private const TOTAL_PRICE = "amount";
     private const DEAL_TYPE = "dealtype";
-    private const ORDER_NUMBER = "ip__ecomm_bridge__order_number";
-    private const DISCOUNT_AMOUNT = "ip__ecomm_bridge__discount_amount";
 
+    public string $orderNumber;
+    public string $orderName;
     public string $orderStage;
     public string $totalPrice;
     public string $dealType;
-    public string $orderNumber;
-    public string $discountAmount;
+    public ?string $discountAmount = null;
     public ?string $discountCode = null;
-    public ?string $dateCreated = null;
+    public ?string $createDate = null;
     public ?string $orderShortNumber = null;
 
     public function __construct()
     {
         parent::__construct();
+        $this->orderNumber = self::ORDER_NUMBER;
+        $this->orderName = self::ORDER_NAME;
         $this->orderStage = self::ORDER_STAGE;
         $this->totalPrice = self::TOTAL_PRICE;
         $this->dealType = self::DEAL_TYPE;
-        $this->orderNumber = self::ORDER_NUMBER;
-        $this->discountAmount = self::DISCOUNT_AMOUNT;
     }
 
     /**
@@ -48,13 +49,14 @@ class OrderSettings extends Model
 
         $orderSettings = new static();
 
+        $orderSettings->orderNumber = $settings->orderNumber ?? self::ORDER_NUMBER;
+        $orderSettings->orderName = $settings->orderName ?? self::ORDER_NAME;
         $orderSettings->orderStage = $settings->orderStage ?? self::ORDER_STAGE;
         $orderSettings->totalPrice = $settings->totalPrice ?? self::TOTAL_PRICE;
         $orderSettings->dealType = $settings->dealType ?? self::DEAL_TYPE;
-        $orderSettings->orderNumber = $settings->orderNumber ?? self::ORDER_NUMBER;
-        $orderSettings->discountAmount = $settings->discountAmount ?? self::DISCOUNT_AMOUNT;
+        $orderSettings->discountAmount = $settings->discountAmount ?? null;
         $orderSettings->discountCode = $settings->discountCode ?? null;
-        $orderSettings->dateCreated = $settings->dateCreated ?? null;
+        $orderSettings->createDate = $settings->createDate ?? null;
         $orderSettings->orderShortNumber = $settings->orderShortNumber ?? null;
 
         return $orderSettings;
@@ -65,14 +67,14 @@ class OrderSettings extends Model
         parent::rules();
 
         return [
-            [['orderStage', 'totalPrice', 'dealType', 'orderNumber', 'discountAmount'], 'required'],
+            [['orderName', 'orderStage', 'totalPrice', 'dealType', 'orderNumber'], 'required'],
+            ['orderNumber', 'compare', 'compareValue' => self::ORDER_NUMBER],
+            ['orderName', 'compare', 'compareValue' => self::ORDER_NAME],
             ['orderStage', 'compare', 'compareValue' => self::ORDER_STAGE],
             ['totalPrice', 'compare', 'compareValue' => self::TOTAL_PRICE],
             ['dealType', 'compare', 'compareValue' => self::DEAL_TYPE],
-            ['orderNumber', 'compare', 'compareValue' => self::ORDER_NUMBER],
-            ['discountAmount', 'compare', 'compareValue' => self::DISCOUNT_AMOUNT],
 
-            [['orderStage', 'totalPrice', 'dealType', 'orderNumber', 'discountAmount', 'discountCode', 'dateCreated', 'orderShortNumber'], 'string'],
+            [array_keys($this->attributes), 'string'],
         ];
     }
 }
